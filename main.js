8318412cd3c08ce3e9c6a4af4e0cce3dea8fe146
jessicafarias <jessica.farias.rosado@gmail.com>
2020-11-16 11:20:01 -0600

    Adding book Form

--- main.js
+++ main.js
@@ -25,10 +25,27 @@ addBookToLibrary(book2);
 addBookToLibrary(book3);
 addBookToLibrary(book4);
 
+function printLibrary(){
+  for (let i in myLibrary){
+    let j = document.createElement('li');
+    j.classList.add('list-group-item');
+    let u = document.createTextNode(myLibrary[i].info());
+    j.appendChild(u);
+    document.getElementById('printing').appendChild(j);
+  }
+}
 
-for (let i in myLibrary){
-  let j = document.createElement('p');
-  let u = document.createTextNode(myLibrary[i].info());
-  j.appendChild(u);
-  document.getElementById('printing').appendChild(j);
+let book;
+
+function NewBook(){
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let pages = document.getElementById('pages').value;
+  let read = document.getElementById('read').value;
+  
+  console.log(title);
+  book = new Book(title, author, pages, read);
+  console.log(book);
+  addBookToLibrary(book);
+  printLibrary();
 }
